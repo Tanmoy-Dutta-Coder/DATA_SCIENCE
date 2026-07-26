80276f381f7435836e8fb53cb32015ee830b2747
Tanmoy Dutta <tanmoydutta0987654321@gmail.com>
2026-07-26 19:44:30 +0530

    Refactor string manipulation functions and add new ones

--- PYTHON/string.py
+++ PYTHON/string.py
@@ -1,3 +1,34 @@
+def letter():
+    x=input("Enter your String :")
+    y=input(f"Enter what is you remove from {x}:")
+    X=x.upper()
+    Y=y.upper()
+    if Y in X:
+         L1=list(X)
+         L1.remove(Y)
+         print(L1)
+    else:
+         print("enter y from  x not other")
+# letter()
+
+def evlen():
+    x=input("Enter Your String:").upper()
+    word=x.split()
+    for i in word:
+        if len(i) %2 ==0:
+            print(i)
+        else:
+            print("No")
+# evlen()
+
+def uphalf():
+    x=input("Enter a String :")
+    if len(x)%2==0:
+        s=x.split()
+        print(s)
+        for i in s:
+           print(i)
+uphalf()
 #uppercase of half string
 def uphalf():
     x=input("Enter a string :")
