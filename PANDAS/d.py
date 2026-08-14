11a3fe2349783a86c098051196bb5cc094914ccf
Tanmoy Dutta <tanmoydutta0987654321@gmail.com>
2026-08-14 21:56:38 +0530

    Add Resampling.ipynb file

--- PANDAS/d.py
+++ PANDAS/d.py
@@ -2,7 +2,7 @@ import pandas as pd
 data={
     "Name":['Tanmoy','Ram','Kumar','Suras',
             'sam'],
-    "Joining_Date":['2005-07-16','2002-12-23','2004-09-27','2006-08-12','2003-03-23'],
+    "Joining_Date":['2005-12-16','2005-12-31','2005-12-27','2005-12-12','2005-12-23'],
     "Joining_Time":['14:23:51','02:12:55','23:45:12','12:22:05','01:45:20'],
     "Marks":[45,78,0,25,87]
     }
@@ -10,20 +10,20 @@ df=pd.DataFrame(data)
 #print(df)
 df["Joining_Date"]=pd.to_datetime(df['Joining_Date'])
 df["Joining_Time"]=pd.to_datetime(df['Joining_Time'])
-df=df.set_index(['Joining_Date','Joining_Time'])
+df=df.set_index('Joining_Date')
 #df=df.set_index()
 print(df)
 #print(df.loc['2005-07-16'])
 
 #Resample
-week=df.resample("W").sum()
-#print(week)
-Month=df.resample("ME").sum()
-#print(Month)
-Year=df.resample("YE").sum()
-#print(Year)
-Day=df.resample("D").sum()
-#print(Day)
+# week=df.resample("W").sum()
+# #print(week)
+# Month=df.resample("ME").sum()
+# #print(Month)
+# Year=df.resample("YE").sum()
+# #print(Year)
+Day=df.resample("D")
+print(Day)
 #Rolling_Window
 df["p"]=df["Marks"].rolling(window=5).mean()
 #print(df)
