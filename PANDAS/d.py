c4a480b0f04fe13a99569cb259fac91e27785f08
Tanmoy Dutta <tanmoydutta0987654321@gmail.com>
2026-08-17 12:20:06 +0530

    solve some questions

--- PANDAS/d.py
+++ PANDAS/d.py
@@ -2,28 +2,28 @@ import pandas as pd
 data={
     "Name":['Tanmoy','Ram','Kumar','Suras',
             'sam'],
-    "Joining_Date":['2005-07-16','2002-12-23','2004-09-27','2006-08-12','2003-03-23'],
+    "Joining_Date":['2005-07-16','2005-12-23','2005-09-27','2005-08-12','2005-03-23'],
     "Joining_Time":['14:23:51','02:12:55','23:45:12','12:22:05','01:45:20'],
     "Marks":[45,78,0,25,87]
     }
 df=pd.DataFrame(data)
 #print(df)
 df["Joining_Date"]=pd.to_datetime(df['Joining_Date'])
-df["Joining_Time"]=pd.to_datetime(df['Joining_Time'])
+# df["Joining_Time"]=pd.to_datetime(df['Joining_Time'])
 df=df.set_index('Joining_Date')
 #df=df.set_index()
 print(df)
 #print(df.loc['2005-07-16'])
 
 #Resample
-week=df.resample("W")
-print(week)
-Month=df.resample("ME")
-print(Month)
-Year=df.resample("YE")
-print(Year)
-Day=df.resample("D")
-print(Day)
+week=df.resample("W").sum()
+print('printing the week ',week)
+# Month=df.resample("ME")
+# print(Month)
+# Year=df.resample("YE")
+# print(Year)
+# Day=df.resample("D")
+# print(Day)
 #Rolling_Window
 df["p"]=df["Marks"].rolling(window=5).mean()
 #print(df)
